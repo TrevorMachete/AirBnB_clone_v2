aeb44fbeb4ef2432e5b4ce23c57d9199bf47f4ee
Trevor Machete <trevormachete@gmail.com>
2024-04-18 21:38:21 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_holberton():
-    """Returns a string at the root route"""
+    """Returns a string at the /airbnb-onepage/ route"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
